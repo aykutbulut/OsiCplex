12205c57b003e7fec68083ce40fcf7952c47ac3f
Aykut Bulut <aykutblt@gmail.com>
2015-05-28 14:37:39 -0400

    buildtools configuration files added.

---
+++ get_dependencies.sh
@@ -0,0 +1,14 @@
+# buildtools
+svn co https://projects.coin-or.org/svn/BuildTools/stable/0.8 BuildTools
+# coinutils
+svn co https://projects.coin-or.org/svn/CoinUtils/stable/2.10/CoinUtils CoinUtils
+# data
+svn co https://projects.coin-or.org/svn/Data/Sample/stable/1.2 Data/Sample
+# ThirdParty
+svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Blas/stable/1.4 ThirdParty/Blas
+svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Lapack/stable/1.5 ThirdParty/Lapack
+svn co https://projects.coin-or.org/svn/BuildTools/ThirdParty/Glpk/stable/1.10 ThirdParty/Glpk
+# get osi
+svn co https://projects.coin-or.org/svn/Osi/stable/0.107/Osi Osi
+# get osi-conic
+git clone https://github.com/aykutbulut/OSI-CONIC OsiConic
