4eae9658669250b7462dfd519bcb7d7d780b862d
Aykut Bulut <aykutblt@gmail.com>
2015-06-24 15:17:52 -0400

    compile script added.

---
+++ compile.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+mkdir build
+cd build
+cplex_inc_dir=/usr/local/mosek/7/tools/platform/linux64x86/h
+cplex_lib_dir=-L/usr/local/mosek/7/tools/platform/linux64x86/bin -lmosek64 -lmosekxx7_0 -lmosekjava7_0 -lmosekscopt7_0 -liomp5
+build_dir=$PWD
+inc_dir=${build_dir%%/}/include
+lib_dir=${build_dir%%/}/lib
+pkg_dir=${lib_dir%%/}/pkgconfig
+PKG_CONFIG_PATH=${pkg_dir}:$PKG_CONFIG_PATH
+export CXXFLAGS="-std=c++11 -g"
+# configure and install CoinUtils
+mkdir CoinUtils
+cd CoinUtils
+../../CoinUtils/configure --prefix=$build_dir
+make -j 10 install
+cd ..
+# configure and install OsiMsk
+mkdir Osi
+cd Osi
+../../Osi/configure --with-cplex-incdir=/usr/local/cplex/include/ilcplex --with-cplex-lib="-L/usr/local/cplex/lib/x86-64_linux/static_pic/ -lcplex -lm -lpthread" --prefix=$build_dir
+make -j 10 install
+cd ..
+#configure and install OsiConic
+mkdir OsiConic
+cd OsiConic
+../../OsiConic/configure --prefix=$build_dir
+make -j 10 install
+cd ..
+#configure and install OsiCplex
+mkdir OsiCplex
+cd OsiCplex
+../../configure --prefix=$build_dir
+make -j 10 install
+cd ..
